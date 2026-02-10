ad0352a62ce2f3c617c81bd391f2bae9530b5a27
Antoni Lubomski <lubomskiantoni24@gmail.com>
2026-02-10 19:07:34 +0100

    Cleanup code and comments

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-/* TO DO
-[x] Ładowanie zadań z local Storage, jeżeli nie ma danych to tworzymy     nową tablice
-[x]Dodawanie zadań z aktualizacją w local storage
-[x]Czyszczenie inputa
-[x]Odznaczanie zadań
-[x]Usuwanie zadań z aktualizacją w local storage
-*/
 
 const addTaskInput = document.getElementById('add-task-input');
 const addBtn = document.getElementById('add-task-button');
 const toDoItemsListElm = document.getElementById('to-do-items-list');
 const removeTaskBtns = document.querySelectorAll('.remove-task-btn');
 
-//przechowywać będzie obiekty które mają id i nazwę
 let toDoList = [];
 if(localStorage.getItem('todolist')) {
   toDoList = JSON.parse(localStorage.getItem('todolist'));
@@ -40,42 +32,29 @@ addTaskInput.addEventListener('keydown', e => {
 addBtn.addEventListener('click', addTask);
 
 function addTask(){
-  //z inputa wysłać na listę i zapisać w local storage
   if(addTaskInput.value){
-  let taskName = addTaskInput.value;
-  addTaskInput.value = '';
-  let taskID = toDoList.length + 1;
-  let isTaskDone = false;
-  toDoList.push({taskName, taskID, isTaskDone});
-  console.log(toDoList);
-  localStorage.setItem('todolist', JSON.stringify(toDoList));
-  addTaskOnSite(taskName, taskID);
+    let taskName = addTaskInput.value;
+    addTaskInput.value = '';
+    let taskID = toDoList.length + 1;
+    let isTaskDone = false;
+    toDoList.push({taskName, taskID, isTaskDone});
+    console.log(toDoList);
+    localStorage.setItem('todolist', JSON.stringify(toDoList));
+    addTaskOnSite(taskName, taskID);
   } else {
     alert("Podaj nazwę zadania");
   }
 }
-
-//nie działa
-// removeTaskBtns.forEach(removeButton => {
-//   console.log("delete obecny");
-//   removeButton.addEventListener('click', () => {
-//     console.log("delete");
-//   })
-// })
-
 
 toDoItemsListElm.addEventListener('click', (event) => {
   if (event.target.classList.contains('remove-task-btn')) {
     let sectionElm = event.target.parentElement;
     const sectionID = sectionElm.id;
 
-   //console.log('kliknieto przycisk w sekcji:', sectionID);
-
     const taskID = parseInt(sectionID.replace('to-do-item-', ''));
 
     sectionElm.remove();
 
-    //zwraca elementy które nie mają id takiego samego jak ten do usunięcia
     toDoList = toDoList.filter(task => task.taskID !== taskID);
     localStorage.setItem('todolist', JSON.stringify(toDoList));
   }
@@ -93,7 +72,7 @@ toDoItemsListElm.addEventListener('click', (e) => {
 
     if(checkboxLabel.classList.contains('done-task')){
       checkboxLabel.classList.remove('done-task');
-      //-1 bo indexy są od 0 a id jest od 1 
+      //ids start from 1, indexes start from 0
       toDoList[taskID - 1].isTaskDone = false; 
       localStorage.setItem('todolist', JSON.stringify(toDoList));
     } else {
